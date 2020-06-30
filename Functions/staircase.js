741a82cb7d94abf4df71712e8fe6d1555b9e5f61
Nazim Uddin <umnazim@gmail.com>
2020-06-30 13:14:01 -0700

    staircase algorithm

---
+++ Functions/staircase.js
@@ -0,0 +1,10 @@
+// staircase consists of # symbols
+function staircase(n) {
+  let filledArray = new Array(n).fill(" ");
+  while (filledArray.indexOf(" ") !== -1) {
+    filledArray.shift();
+    filledArray.push("#");
+    console.log(filledArray.join(""));
+    n--;
+  }
+}
